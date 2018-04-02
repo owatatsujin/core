8c5a046fdfb55592d493a10438b518ce75f2be83
pastatsh <wakyubabu@gmail.com>
2018-04-02 20:39:37 +0900

    test

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/// <reference path="../phaser.d.ts"/>
+import "phaser";
+
+export class MainScene extends Phaser.Scene {
+    private phaserSprite: Phaser.GameObjects.Sprite;
+  
+    preload(): void {
+      this.load.image("logo", "https://avatars1.githubusercontent.com/u/38001038?s=400&u=3d2b8655cdb52443b6ecefca664c83863d4336c8&v=4");
+    }
+  
+    create(): void {
+      this.phaserSprite = this.add.sprite(400, 300, "logo");
+    }
+}
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,12 @@
 /// <reference path="../phaser.d.ts"/>
 
 import "phaser";
+import { MainScene } from "./main";
 
 var game = new Phaser.Game({
     width: 800,
     height: 600,
     type: Phaser.WEBGL
 } as GameConfig);
+
+game.scene.add("main", MainScene, true);
