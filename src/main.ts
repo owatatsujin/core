6eb9cc7e93e70157fe6998fff1c352cd24a00b1e
pastatsh <wakyubabu@gmail.com>
2018-04-03 10:24:21 +0900

    インデント修正

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,13 @@ export class MainScene extends Phaser.Scene {
     private data: Data;
 
     preload(): void {
-      this.load.text("data", "data.txt");
-      this.load.image("logo", "https://avatars1.githubusercontent.com/u/38001038?s=400&u=3d2b8655cdb52443b6ecefca664c83863d4336c8&v=4");
+        this.load.text("data", "data.txt");
+        this.load.image("logo", "https://avatars1.githubusercontent.com/u/38001038?s=400&u=3d2b8655cdb52443b6ecefca664c83863d4336c8&v=4");
     }
 
     create(): void {
-      this.phaserSprite = this.add.sprite(400, 300, "logo");
-      this.data = new Data(this.cache.text.get("data"));
+        this.phaserSprite = this.add.sprite(400, 300, "logo");
+        this.data = new Data(this.cache.text.get("data"));
     }
 
     update(): void {
